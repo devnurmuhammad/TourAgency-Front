eaa78c7f53b3c6e4144879dc3a879a44fa514a03
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-10-10 12:18:08 +0500

    home component is created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 export const routes: Routes = [
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutUsComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path:'**', redirectTo: '/home'}
 ];
