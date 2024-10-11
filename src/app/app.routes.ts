6b0976d882c0ee787c657987a1f3408ddfaad37a
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-10-11 21:59:44 +0500

    nothing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutUsComponent},
+    {path: 'contacts', component: ContactsComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path:'**', redirectTo: '/home'}
 ];
